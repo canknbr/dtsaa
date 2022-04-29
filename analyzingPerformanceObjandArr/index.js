54f22cd13debde5e13b7841e0aee5cb88b1bbb43
Can Kanbur <cankanbur1@gmail.com>
2022-04-29 10:57:26 +0300

    .

---
+++ analyzingPerformanceObjandArr/index.js
@@ -0,0 +1,44 @@
+// bigO of objects
+/*
+insertion - O(1)
+deletion - O(1)
+search - O(n)
+access - O(1)
+
+
+obj methods
+ob.keys() - O(n)
+ob.values() - O(n)
+ob.entries() - O(n)
+ob.hasOwnProperty() - O(1)
+ob.propertyIsEnumerable() - O(1)
+ob.isPrototypeOf() - O(1)
+ob.toString() - O(1)
+ob.toLocaleString() - O(1)
+ob.valueOf() - O(1)
+ 
+*/
+
+
+/*
+big o of arrays
+insertion - in depends
+deletion - in depends
+search - O(n)
+access - O(1)
+
+
+arr methods
+arr.concat() - O(n)
+arr.join() - O(n)
+arr.pop() - O(1)
+arr.push() - O(1)
+arr.reverse() - O(n)
+arr.shift() - O(n)
+arr.slice() - O(n)
+arr.sort() - O(n log n)
+arr.splice() - O(n)
+arr.unshift() - O(n)
+arr.indexOf() - O(n)
+(forEach, map, filter, reduce, every, some, reduceRight) - O(n)
+*/
