8f9970e47dc611d8aa49e59499bf9548e5b78bf3
Can Kanbur <cankanbur1@gmail.com>
2022-04-29 09:59:49 +0300

    init

---
+++ bigO/main.js
@@ -0,0 +1,13 @@
+// sample 1
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// sample 2
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
